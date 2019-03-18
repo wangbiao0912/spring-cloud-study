83313c4a6be88ff93d995044fc03dff6e15c2419
wangbiao <wangbiao9012@gmail.com>
2019-03-18 18:09:00 +0800

    '框架搭建'

--- service-zuul/src/main/java/com/after00/MyFilter.java
+++ service-zuul/src/main/java/com/after00/MyFilter.java
@@ -1,4 +1,4 @@
-package com.forezp;
+package com.after00;
 
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
@@ -48,4 +48,14 @@ public class MyFilter extends ZuulFilter{
         log.info("ok");
         return null;
     }
+    /**
+     filterType：返回一个字符串代表过滤器的类型，在zuul中定义了四种不同生命周期的过滤器类型，具体如下：
+     pre：路由之前
+     routing：路由之时
+     post： 路由之后
+     error：发送错误调用
+     filterOrder：过滤的顺序
+     shouldFilter：这里可以写逻辑判断，是否要过滤，本文true,永远过滤。
+     run：过滤器的具体逻辑。可用很复杂，包括查sql，nosql去判断该请求到底有没有权限访问。
+     * */
 }
